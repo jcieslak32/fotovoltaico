40116f14bdaee9ad31c5bf025be18ac1e4642230
Yago Lagrotti Bracco <yago.lagrotti@outlook.com>
2024-03-20 10:40:36 -0300

    fix: hosting changes

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ const schedule = require("node-schedule")
 const app = express()
 
 app.use(express.json())
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
+app.use(cors({ credentials: true, origin: "https://front-tcv0.onrender.com" }))
 app.use(express.static("public"))
 app.use("/api/v1", router)
 
 schedule.scheduleJob("*/59 * * * *", async () => {
     try {
         await axios.get(
-            "http://localhost:3001/api/v1/set-station-generation-to-db"
+            "https://api-skzm.onrender.com/api/v1/set-station-generation-to-db"
         )
     } catch (error) {
         console.error("Error while calling the endpoint:", error.message)
     }
 })
 
-app.listen(3001)
+app.listen(3000)
