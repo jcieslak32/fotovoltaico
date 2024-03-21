85372c482749fb309e7fef0b1a923fdf5ce3229d
Yago Lagrotti Bracco <yago.lagrotti@outlook.com>
2024-03-21 15:45:06 -0300

    feat: done first project

--- src/app/config/db.js
+++ src/app/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 async function main() {
     await mongoose.connect(
-        "mongodb+srv://admin:ZE6rR1jCYnGTpQCL@cluster0.ja04jx0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.ja04jx0.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_APPNAME}`
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ const schedule = require("node-schedule")
 const app = express()
 
 app.use(express.json())
-app.use(cors({ credentials: true, origin: "https://front-na66.onrender.com" }))
+app.use(cors({ credentials: true, origin: process.env.FRONT_BASE_URL }))
 app.use(express.static("public"))
 app.use("/api/v1", router)
 
 schedule.scheduleJob("*/59 * * * *", async () => {
     try {
         await axios.get(
-            "https://api-skzm.onrender.com/api/v1/set-station-generation-to-db"
+            `${process.env.API_BASE_URL}/set-station-generation-to-db`
         )
     } catch (error) {
         console.error("Error while calling the endpoint:", error.message)
     }
 })
 
-app.listen(3000)
+app.listen(process.env.API_BASE_PORT)
